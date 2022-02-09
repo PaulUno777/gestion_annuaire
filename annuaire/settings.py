adc7c655e73474169edc503d5da9ca164b111fbe
thierryDEMBABA <67897735+thierryDEMBABA@users.noreply.github.com>
2022-02-09 12:35:07 +0100

    Update setting.py(personnalisation of auth system)

--- annuaire/settings.py
+++ annuaire/settings.py
@@ -121,3 +121,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#adding personal user auth model
+AUTH_USER_MODEL = 'mainApp.User'
+LOGIN_REDIRECT_URL = 'accueil'
